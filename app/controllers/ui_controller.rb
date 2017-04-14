b9f1f0c557fa3f5ebd000a81b5fca6f56c39239a
Joseph Tooley <joseph.tooley@gmail.com>
2017-04-13 20:51:38 -0400

    Add redirect

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -1,7 +1,2 @@
 class UiController < ApplicationController
-
-  def index
-    redirect_to "/client/index.html"
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,5 @@ Rails.application.routes.draw do
   end
 
   get "/client-assets/:name.:format", :to => redirect("/client/client-assets/%{name}.%{format}")
-  get '/ui' => 'ui#index'
-  get '/ui#' => 'ui#index'
-
-  root "ui#index"
+  get "/", :to => redirect("/client/index.html")
 end
