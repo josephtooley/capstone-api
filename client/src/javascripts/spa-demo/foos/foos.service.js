d806a0ecc174cd8f1f7c225db6f24c36fb07fafa
Joseph Tooley <joseph.tooley@gmail.com>
2017-04-22 16:10:54 -0400

    Create, update, and delete foos via external app

--- client/src/javascripts/spa-demo/foos/foos.service.js
+++ client/src/javascripts/spa-demo/foos/foos.service.js
@@ -7,12 +7,17 @@
 
   FooFactory.$inject = [ "$resource", "spa-demo.APP_CONFIG" ];
   function FooFactory($resource, APP_CONFIG) {
-    return $resource(APP_CONFIG.server_url + "/api/foos?:id",
+    return $resource(APP_CONFIG.server_url + "/api/foos/:id",
       { id: '@id' },
       {
-        update: { method: "PUT" }
+        update: { method: "PUT", transformRequest: buildNestedBody },
+        save: { method: "POST", transformRequest: buildNestedBody }
       }
     );
+  }
+
+  function buildNestedBody(data) {
+    return angular.toJson({foo: data})
   }
 
 })();
