26c07b0534d0f2af23bc61a48c3aad2a755f972d
Joseph Tooley <joseph.tooley@gmail.com>
2017-04-29 14:57:20 -0400

    Add spa app

---
+++ app/assets/javascripts/spa.js
@@ -0,0 +1,11 @@
+//test javascript
+//= require jquery2
+//= require bootstrap
+//= require angular
+//= require angular-ui-router
+//= require angular-resource
+
+//= require spa/app.module
+//= require spa/app.router
+//= require spa/app.constant
+//= require spa/cities/cities.module
---
+++ app/assets/javascripts/spa/app.module.js
@@ -0,0 +1,10 @@
+(function () {
+  "use strict";
+
+  angular
+    .module("spa", [
+      "ui.router",
+      "spa.cities"
+    ]);
+
+})();
